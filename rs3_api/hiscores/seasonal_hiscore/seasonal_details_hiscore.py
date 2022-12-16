448ff7d57ea7e4f7d7d2f2870e15fed8669c4940
Luciano Feder <lucianoo.feder@gmail.com>
2022-12-16 09:43:29 -0300

    imrpovement: created a simple cache service

--- rs3_api/hiscores/seasonal_hiscore/seasonal_details_hiscore.py
+++ rs3_api/hiscores/seasonal_hiscore/seasonal_details_hiscore.py
@@ -9,21 +9,20 @@ from rs3_api.utils.const import BASE_URL
 
 @dataclass
 class SeasonalDetailsHiscore:
-    __api_data: str = field(default='', repr=False)
+    __api_data: dict = field(default_factory=dict, repr=False)
+    __last_call: str = field(default='', repr=False)
 
 
     @property
     def current(self) -> List[SeasonalEvent]:
-        return self.__get_data()
+        return self.__get_formatted_data()
 
     @property
     def all(self) -> List[SeasonalEvent]:
-        return self.__get_data(True)
+        return self.__get_formatted_data(True)
 
-    def __get_data(self, archived: bool = False) -> List[SeasonalEvent]:
-        is_archived = '?status=archived' if archived else ''
-        if not self.__api_data:
-            res = requests.get(f'{BASE_URL}/m=temp-hiscores/getHiscoreDetails.json{is_archived}')
+    def __get_formatted_data(self, archived: bool = False) -> List[SeasonalEvent]:
+        res = self.__get_data(archived)
         return [SeasonalEvent(
             start_date=data.get('startDate'),
             end_date=data.get('endDate'),
@@ -36,4 +35,14 @@ class SeasonalDetailsHiscore:
             status=data.get('status'),
             type=data.get('type'),
             id=data.get('id')
-        ) for data in res.json()] 
+        ) for data in res] 
+
+    def __get_data(self, archived: bool = False) -> dict:
+        current_call = f'archived: {archived}'
+        if getattr(self, '__api_data', None) and current_call == self.__last_call:
+            return self.__api_data
+        is_archived = '?status=archived' if archived else ''
+        res = requests.get(f'{BASE_URL}/m=temp-hiscores/getHiscoreDetails.json{is_archived}')
+        self.__api_data = res.json()
+        return self.__api_data
+        
