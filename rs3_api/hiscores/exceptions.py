a2c4ebd3ebfc4ead5a206bf82a3022e93b9608f0
Luciano Feder <lucianoo.feder@gmail.com>
2022-12-16 12:17:30 -0300

    style: fixed typos and trailing whitespaces

--- rs3_api/hiscores/exceptions.py
+++ rs3_api/hiscores/exceptions.py
@@ -2,6 +2,7 @@ class UserNotFoundException(Exception):
     def __init__(self, username: str) -> None:
         self.message = f'User "{username} "was not found'
 
+
 class ClanNotFoundException(Exception):
     def __init__(self, clan_name: str) -> None:
         self.message = f'Clan "{clan_name} "was not found'
